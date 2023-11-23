41f5d21a272155e518c95b789034d756603cb3e7
vijenderchimma <vijenderchimma424@gmail.com>
2023-11-23 12:52:21 +0000

    react hooks greeting

---
+++ src/components/GreetUser/index.js
@@ -0,0 +1,27 @@
+import {useState} from 'react'
+import {
+  MainContainer,
+  NameInput,
+  MsgContent,
+  NameText,
+} from './styledComponents'
+
+const GreetUser = () => {
+  const [name, setName] = useState('')
+  const onChangeName = event => setName(event.target.value)
+
+  return (
+    <MainContainer>
+      <NameInput
+        type="text"
+        onChange={onChangeName}
+        placeholder="Your name"
+        value={name}
+      />
+      <MsgContent>
+        Hello <NameText>{name}</NameText>
+      </MsgContent>
+    </MainContainer>
+  )
+}
+export default GreetUser
